5cb6479456e08b064b336b218435d91daf9db1ab
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-12-26 18:24:32 +0500

    Time: 11 ms (60.48%) | Memory: 69.6 MB (37.23%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> num1;
-        unordered_map<int,int> num2;
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
         int answer1 = 0;
         int answer2 = 0;
 
-        for(int i:nums1){
-            num1[i]++;
-        }
-
-        for(int j:nums2){
-            num2[j]++;
-        }
-
-        for(auto x:num1){
-            if(num2.find(x.first) != num2.end()){
-                answer1 += x.second;
+        for (int i=0; i<nums1.size(); i++) {
+            if (set2.count(nums1[i])) {
+                answer1++;
             }
         }
 
-        for(auto y:num2){
-            if(num1.find(y.first) != num1.end()){
-                answer2 += y.second;
+        for (int j=0; j<nums2.size(); j++) {
+            if (set1.count(nums2[j])) {
+                answer2++;
             }
         }
 
-        return {answer1,answer2};
+        return {answer1, answer2};
     }
 };
