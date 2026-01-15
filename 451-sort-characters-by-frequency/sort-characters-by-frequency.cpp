ea9e7f8e1b60f57e739fa7052881935df4537035
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2026-01-15 21:58:48 +0500

    Time: 65 ms (5.01%) | Memory: 10.4 MB (95.25%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>freq;
+
+        for(auto i:s){
+            freq[i]++;
+        }
+
+        sort(s.begin(),s.end(),[&](char a, char b){
+            if(freq[a] == freq[b]){
+                return a > b;
+            }
+            return freq[a] > freq[b];
+        });
+
+        return s;
+    }
+};
