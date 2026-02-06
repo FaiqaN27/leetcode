0660cf7757da86b7d8fb68ce275caa6b2ed8b587
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2026-02-06 22:37:27 +0500

    Time: 0 ms (100.00%) | Memory: 23.1 MB (99.13%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-bool isPalindromic(string& word){
-    int left = 0; 
-    int right = word.size()-1;
-    while(left<=right){
-        if(word[left] == word[right]){
+    bool isPalindromic(string& word) {
+        int left = 0;
+        int right = word.size() - 1;
+        while (left <= right) {
+            if (word[left] != word[right]) {
+                return false;
+            }
             left++;
             right--;
         }
-        else{
-            return false;
-        }
+        return true;
     }
-    return true;
-}
     string firstPalindrome(vector<string>& words) {
-        
-        for(int i=0;i<words.size();i++){
-            if(isPalindromic(words[i])){
+
+        for (int i = 0; i < words.size(); i++) {
+            if (isPalindromic(words[i])) {
                 return words[i];
             }
         }
